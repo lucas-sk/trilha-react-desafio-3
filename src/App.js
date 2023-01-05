caea9a92ecf58eca47316f970dbceed247396d99
lusk <lucas78654321@outlook.com>
2023-01-05 10:49:04 -0300

    add signup on site

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
+  BrowserRouter as Router, Route, Routes
 } from "react-router-dom";
 import { Feed } from "./pages/feed";
 
-import { Home } from './pages/home'
-import { Login } from './pages/login'
+import { Home } from './pages/home';
+import { Login } from './pages/login';
+import { SignUp } from './pages/signup';
 import { GlobalStyle } from './styles/global';
 
 function App() {
@@ -16,6 +15,7 @@ function App() {
      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/feed" element={<Feed />} />
      </Routes >
     </Router>
